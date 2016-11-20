6858337bc00c396442ded6e0d82bddac3958ad13
nobmr <274925658@qq.com>
2016-11-20 23:51:10 +0800

    upt:freemarker框架引入

--- src/main/java/com/crm/controller/IndexController.java
+++ src/main/java/com/crm/controller/IndexController.java
@@ -1,8 +1,17 @@
 package com.crm.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/site")
 public class IndexController {
 	
+	@RequestMapping("/main")
+	public String main(HttpServletRequest request,Model model){
+		return "main";
+	}
 }
