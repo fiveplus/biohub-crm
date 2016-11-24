5f7e36224efd5a51fc97ab7c5fa4ccb9c03401fa
nobmr <274925658@qq.com>
2016-11-24 11:35:09 +0800

    add:权限ajax shiro更新

--- src/main/java/com/crm/controller/admin/IndexAdminController.java
+++ src/main/java/com/crm/controller/admin/IndexAdminController.java
@@ -1,6 +1,8 @@
 package com.crm.controller.admin;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.crm.controller.admin.bo.LogBO;
@@ -94,6 +97,14 @@ public class IndexAdminController {
         SecurityUtils.getSubject().logout();    
         redirectAttributes.addFlashAttribute("message", "您已安全退出");    
         return "login";
+	}
+	
+	@RequestMapping("/admin/msg")
+	public @ResponseBody Map<String,Object> msg(HttpServletRequest request,Model model){
+		Map<String,Object> returnMap = new HashMap<String,Object>();
+		returnMap.put("code", 4);
+		returnMap.put("msg", "授权验证失败。");
+		return returnMap;
 	}
 	
 	/**
---
+++ src/main/java/com/crm/filter/RoleAuthorizationFilter.java
@@ -0,0 +1,83 @@
+package com.crm.filter;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.CollectionUtils;
+import org.apache.shiro.util.StringUtils;
+import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
+import org.apache.shiro.web.filter.authz.AuthorizationFilter;
+import org.apache.shiro.web.util.WebUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RoleAuthorizationFilter extends AuthorizationFilter {
+	private static final Logger log = LoggerFactory.getLogger(RoleAuthorizationFilter.class); 
+	
+	protected boolean onAccessDenied(ServletRequest req,
+			ServletResponse res, Object mappedValue) throws Exception {
+	
+		HttpServletRequest request = (HttpServletRequest) req;  
+        HttpServletResponse response = (HttpServletResponse) res; 
+        Subject subject = getSubject(request, response);
+        if(subject.getPrincipal() == null){
+        	//判断是否为ajax
+        	if("XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With"))){
+        		//没有登录无权
+        		response.setCharacterEncoding("UTF-8");
+        		PrintWriter out = response.getWriter();
+        		out.print("{code:4,msg:'请先登录。'}");
+        		out.flush();
+    			out.close();
+        	}else{
+        		saveRequestAndRedirectToLogin(request, response);
+        	}
+        }else{
+        	if("XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With"))){
+        		//登录无权
+        		response.setCharacterEncoding("UTF-8");
+        		PrintWriter out = response.getWriter();
+        		out.print("{code:4,msg:'没有授权。'}");
+        	}else{
+        		String unauthorizedUrl = getUnauthorizedUrl();
+        		if (StringUtils.hasText(unauthorizedUrl)) {  
+                    WebUtils.issueRedirect(request, response, unauthorizedUrl);  
+                } else {  
+                    WebUtils.toHttp(response).sendError(401);  
+                }  
+        	}
+        }
+        return false;
+	}
+	
+	public boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue)  
+            throws IOException {  
+  
+        Subject subject = getSubject(request, response);  
+        String[] rolesArray = (String[]) mappedValue;  
+  
+        if (rolesArray == null || rolesArray.length == 0) {  
+            // no roles specified, so nothing to check - allow access.  
+            return true;  
+        }  
+  
+        Set<String> roles = CollectionUtils.asSet(rolesArray);  
+        for (String role : roles) {  
+            if (subject.hasRole(role)) {  
+                return true;  
+            }  
+        }  
+        return false;  
+    }  
+
+	
+
+}
