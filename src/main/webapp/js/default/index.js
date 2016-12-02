506f0be0e0e5c8be2f365170481ea0b142667054
nobmr <274925658@qq.com>
2016-12-02 12:00:47 +0800

    upt:修改查询页。

--- src/main/webapp/js/default/index.js
+++ src/main/webapp/js/default/index.js
@@ -9,3 +9,22 @@ function search_focus(flag){
 		div.css("border","1px solid #eee");
 	}
 }
+$(document).ready(function(){
+	var navH = $(".search-index").offset().top;
+	//滚动条事件
+	$(window).scroll(function(){
+		//获取滚动条的滑动距离
+		var scroH = $(this).scrollTop();
+		//滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+		if(scroH==0){
+			$(".search-index").css({"position":"fixed","-webkit-box-shadow":"0 0 0"});
+		}else{
+			if(scroH>=navH){
+				$(".search-index").css({"-webkit-box-shadow":"0 0 5px rgba(81, 203, 238, 1)"});
+			}else if(scroH < navH){
+				$(".search-index").css({"position":"fixed","-webkit-box-shadow":"0 0 0"});
+			}
+		}
+		
+	});
+});
